49dfc6ec152d708f63268ee8efaf2e55bbead688
Ravi Kanaujiya <ravi.kanaujiya@zopsmart.com>
2022-01-20 17:24:32 +0530

    assignments_Day2

---
+++ assignments_2/concat.js
@@ -0,0 +1,9 @@
+const koncat=function(a)
+{
+    if(b)
+   return return konkat(a+b);
+   elsereturn a;
+
+}
+console.log(konkat("r")("i")("y")("a")());
+console.log(konkat("t")("h")("a")("r")("u")("n"));
