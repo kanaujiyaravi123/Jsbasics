ca7b3799f7ad77a5f0433356351ee29d66801d41
Ravi Kanaujiya <ravi.kanaujiya@zopsmart.com>
2022-01-24 16:01:20 +0530

    assignment-day5

---
+++ Day5/Promise.js
@@ -0,0 +1,25 @@
+const api={
+    '1245':{
+        U_Deatil:{
+            U_amount:5000
+        }
+    }
+}
+function getUserdetail(uid)
+{
+    let dummy={};
+        return new Promise((resolve,reject)=>resolve(dummy=api[uid]))
+}
+function Acc_details(par){
+    let dummy2={};
+    return new Promise((resolve,reject)=>resolve(dummy2=par.U_Deatil))
+}
+function Deduct(paisa){
+    if(paisa.U_amount>500){
+        console.log("amount deducted");
+    }
+    else {
+    console.log("need more money");
+    }
+}
+getUserdetail("1245").then((d1)=>Acc_details(d1).then((d2)=>Deduct(d2))).catch(()=>console.log("error"));
